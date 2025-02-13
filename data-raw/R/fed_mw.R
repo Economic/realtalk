6ba5a1f2955f24edb858a4171afa75f9a6fb7576
Ben Zipperer <benzipperer@gmail.com>
2025-02-13 13:05:10 -0500

    update min wage data through 2025

--- data-raw/R/fed_mw.R
+++ data-raw/R/fed_mw.R
@@ -5,13 +5,12 @@ create_fed_mw_annual <- function(raw_csv) {
   mw_csv <- "data-raw/processed/us_minimum_wage_annual.csv"
   mw_rda <- "data/us_minimum_wage_annual.rda"
 
-  years_data <- enframe(1938:2023, name = NULL, value = "year")
+  years_data <- enframe(1938:2025, name = NULL, value = "year")
 
   us_minimum_wage <- raw_csv %>%
     read_csv(show_col_types = FALSE) %>%
     mutate(year = year(mdy(date))) %>%
     summarize(minimum_wage = max(fed_min_wage), .by = year) %>%
-    add_row(year = 2024, minimum_wage = 7.25) %>%
     full_join(years_data, by = "year") %>%
     arrange(year) %>%
     fill(minimum_wage)
@@ -29,7 +28,7 @@ create_fed_mw_monthly <- function(raw_csv) {
   mw_csv <- "data-raw/processed/us_minimum_wage_monthly.csv"
   mw_rda <- "data/us_minimum_wage_monthly.rda"
 
-  final_yearmonth_date <- "2024m12"
+  final_yearmonth_date <- "2025m12"
   current_fed_mw <- 7.25
 
   us_minimum_wage_monthly <- raw_csv %>%
